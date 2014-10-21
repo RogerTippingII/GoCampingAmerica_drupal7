a205515a14f4b859e7a3bd3dbb88d4ac91d2ecce
mauro <mauro@blackboxargentina.com>
2014-10-21 16:57:31 -0300

    new ad script on top block from homepage

--- sites/all/modules/gca_search/blocks/home_ad_left.php
+++ sites/all/modules/gca_search/blocks/home_ad_left.php
@@ -1,11 +1,11 @@
 <div id="ad-300">
-  <!-- Home - 300x250 [javascript] -->
-  <script type="text/javascript">
-    var rnd = window.rnd || Math.floor(Math.random()*10e6);
-    var pid154953 = window.pid154953 || rnd;
-    var plc154953 = window.plc154953 || 0;
-    var abkw = window.abkw || '';
-    var absrc = 'http://ab153757.adbutler-zilon.com/adserve/;ID=153757;size=300x250;setID=154953;type=js;kw='+abkw+';pid='+pid154953+';place='+(plc154953++)+';rnd='+rnd+'';
-    document.write('<scr'+'ipt src="'+absrc+'" type="text/javascript"></scr'+'ipt>');
-  </script>
+<!-- 300x250 [async] -->
+<script type="text/javascript">if (!window.AdButler){(function(){var s = document.createElement("script"); s.async = true; s.type = "text/javascript";s.src = 'http://ab153757.adbutler-zilon.com/app.js';var n = document.getElementsByTagName("script")[0]; n.parentNode.insertBefore(s, n);}());}</script>
+<script type="text/javascript">
+var AdButler = AdButler || {}; AdButler.ads = AdButler.ads || [];
+var abkw = window.abkw || '';
+var plc173533 = window.plc173533 || 0;
+document.write('<div id="placement_173533_'+plc173533+'"></div>');
+AdButler.ads.push({handler: function(opt){ AdButler.register(153757, 173533, [300,250], 'placement_173533_'+opt.place, opt); }, opt: { place: plc173533++, keywords: abkw, domain: 'ab153757.adbutler-zilon.com' }});
+</script>
 </div> <!-- /ad-300 -->
